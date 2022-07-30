f70a380e5a16a3eb2de2e86dbb3b5da551b215c8
André Lubawski <andrelubawski97@outlook.com>
2022-07-30 10:54:28 -0300

    typedef

---
+++ typedefExemplo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+    typedef float nota;
+
+    nota prova1 = 7.0;
+    nota prova2 = 6.0;
+
+    nota soma = prova1 + prova2;
+
+    printf("A soma das notas eh %.2f\n", soma);
+
+return 0;
+}
