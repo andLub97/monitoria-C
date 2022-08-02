f86baf2b81e989f5cbdae8f72d801d9664702ec3
André Lubawski <andrelubawski97@outlook.com>
2022-08-02 12:09:32 -0300

    select sort

---
+++ ordenacaoDeVetoresexemplo01.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+void select_sort (int vetor[], int tam){
+    int menor, troca;
+
+    for(int i = 0; i < (tam -1); i++){
+        menor = i;
+        for(int j = (i+1); j < tam; j++){
+            if(vetor[j]<vetor[menor]){
+                menor = j;
+            }
+        }
+        if(i != menor){
+            troca = vetor[i];
+            vetor[i] = vetor[menor];
+            vetor[menor] = troca;
+        }
+    }
+}
+
+
+int main(){
+
+ //select sort
+
+    int vetor[6] = {8, 3, 1, 42, 12, 5};
+
+    select_sort(vetor, 6);
+
+    for(int i = 0; i < 6; i++){
+        printf("%d\n", vetor[i]);
+    }
+
+return 0;
+}
